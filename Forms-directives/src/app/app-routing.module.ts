58913a1da0ac007b5c12be94cc3f0a75e6a708c1
Mamatha574 <mamathamalagona123@gmail.com>
2020-04-30 09:20:06 +0530

    first commit

---
+++ Forms-directives/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { StructuralDirectivesComponent } from './structural-directives/structural-directives.component';
+import { TemplateDrivenFormsComponent } from './template-driven-forms/template-driven-forms.component';
+import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
+
+
+
+const routes: Routes = [
+  {path:'',redirectTo:'/structural-directives',pathMatch:'full'},
+  { path:'structural-directives',component:StructuralDirectivesComponent },
+  { path: 'tdf',component:TemplateDrivenFormsComponent},
+  { path:'mdf',component:ReactiveFormsComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Forms-directives/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import{ FormsModule,ReactiveFormsModule} from '@angular/forms'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TemplateDrivenFormsComponent } from './template-driven-forms/template-driven-forms.component';
+import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
+import { StructuralDirectivesComponent } from './structural-directives/structural-directives.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    TemplateDrivenFormsComponent,
+    ReactiveFormsComponent,
+    StructuralDirectivesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
